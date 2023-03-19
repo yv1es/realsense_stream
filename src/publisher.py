2d028b7e4373ca585ace3367186f19314048ef2b
yv1es <yvesinglin@gmail.com>
2023-03-19 22:07:42 +0100

    publisher

--- src/publisher.py
+++ src/publisher.py
@@ -1,6 +1,40 @@
 #!/usr/bin/env python3
+# Software License Agreement (BSD License)
+#
+# Copyright (c) 2008, Willow Garage, Inc.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+#  * Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+#  * Redistributions in binary form must reproduce the above
+#    copyright notice, this list of conditions and the following
+#    disclaimer in the documentation and/or other materials provided
+#    with the distribution.
+#  * Neither the name of Willow Garage, Inc. nor the names of its
+#    contributors may be used to endorse or promote products derived
+#    from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+# COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+#
+# Revision $Id$
 
-
+## Simple talker demo that published std_msgs/Strings messages
+## to the 'chatter' topic
 
 import rospy
 from std_msgs.msg import String
@@ -34,6 +68,31 @@ def setup_sockets():
     return socket_c
 
 
+def makeCamearInfo():
+
+    # Define camera parameters
+    camera_matrix = [618.9998779296875, 0.0, 320.2127685546875,
+                    0.0, 618.9998779296875, 239.30780029296875,
+                    0.0, 0.0, 1.0]
+    distortion_coefficients = [0.0, 0.0, 0.0, 0.0, 0.0]
+    width = 640
+    height = 480
+
+    # Create CameraInfo message
+    camera_info = CameraInfo()
+    camera_info.header.frame_id = "camera_frame"
+    camera_info.width = FRAME_WIDTH
+    camera_info.height = FRAME_HEIGHT
+    camera_info.distortion_model = "plumb_bob"
+    camera_info.D = distortion_coefficients
+    camera_info.K = camera_matrix
+    camera_info.R = [1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0]
+    camera_info.P = [618.9998779296875, 0.0, 320.2127685546875, 0.0,
+                    0.0, 618.9998779296875, 239.30780029296875, 0.0,
+                    0.0, 0.0, 1.0, 0.0]
+
+    return camera_info
+
 
 
 def talker():
@@ -44,11 +103,8 @@ def talker():
 
 
     rospy.init_node('camera_node', anonymous=True)
-    rate = rospy.Rate(20) # 10hz
 
-    camera_info = CameraInfo()
-    camera_info.height = FRAME_HEIGHT
-    camera_info.width = FRAME_WIDTH
+    camera_info = makeCamearInfo()
 
     rospy.loginfo("Waiting for connection")
     socket = setup_sockets()
@@ -83,13 +139,16 @@ def talker():
         color_ros = bridge.cv2_to_imgmsg(color_image, encoding="rgb8")
         depth_ros = bridge.cv2_to_imgmsg(depth_image, encoding="mono16")
         
+        # color_ros.is_bigendian = True
+        # depth_ros.is_bigendian = True
 
         current_time = rospy.get_time()
-        header = Header(stamp=rospy.Time.from_sec(current_time), frame_id="base_link")
+        header = Header(stamp=rospy.Time.from_sec(current_time), frame_id="camera_link")
 
         color_ros.header = header
         depth_ros.header = header
         camera_info.header = header
+
 
         color_pub.publish(color_ros)
         depth_pub.publish(depth_ros)
