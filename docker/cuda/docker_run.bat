2fdc74df79fa2f4659e14397f5350cc77d582ac7
yv1es <yvesinglin@gmail.com>
2023-04-07 10:58:47 +0200

    docker

--- docker/cuda/docker_run.bat
+++ docker/cuda/docker_run.bat
@@ -1,3 +1,3 @@
 @echo off 
 echo Running container
-docker run -it -p 5000:5000 -e DISPLAY=host.docker.internal:0.0 --gpus all opencv-gpu bash
+docker run -it -p 5000:5000 -e DISPLAY=host.docker.internal:0.0 --gpus all opencv_gpu bash
